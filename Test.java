ad3f13effecfd623c1a762adff12f287e4deb37c
ilgimaltay <ilgim_altay@hotmail.com>
2025-11-28 21:14:44 +0100

    Calculator

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+
+        int a = 10 , b = 20 , c = 1;
+
+        if (a<b && a<c){
+            System.out.println("A en kucuk sayidir");
+
+        }else {
+            System.out.println("A en kucuk sayi degildir");
+
+        }
+
+    }
+}
